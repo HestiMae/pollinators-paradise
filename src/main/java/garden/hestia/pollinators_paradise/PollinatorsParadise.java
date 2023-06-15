27e2d196c986c5b4c19223a030c3636c8a3d586e
HestiMae <hestiaheartstrings@gmail.com>
2023-06-15 15:38:57 +1000

    added apiarist suit item

--- src/main/java/garden/hestia/pollinators_paradise/PollinatorsParadise.java
+++ src/main/java/garden/hestia/pollinators_paradise/PollinatorsParadise.java
@@ -17,8 +17,9 @@ public class PollinatorsParadise implements ModInitializer {
 	public static final String ID = "pollinators_paradise";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 	public static final ApiaristArmorMaterial APIARIST_ARMOR_MATERIAL = new ApiaristArmorMaterial();
-	public static final Item APIARIST_VEIL = new HoneyableArmorItem(APIARIST_ARMOR_MATERIAL, ArmorItem.ArmorSlot.HELMET,new QuiltItemSettings());
-	public static final Item APIARIST_WELLIES = new HoneyableArmorItem(APIARIST_ARMOR_MATERIAL, ArmorItem.ArmorSlot.BOOTS,new QuiltItemSettings());
+	public static final Item APIARIST_VEIL = new HoneyableArmorItem(APIARIST_ARMOR_MATERIAL, ArmorItem.ArmorSlot.HELMET, new QuiltItemSettings());
+	public static final Item APIARIST_WELLIES = new HoneyableArmorItem(APIARIST_ARMOR_MATERIAL, ArmorItem.ArmorSlot.BOOTS, new QuiltItemSettings());
+	public static final Item APIARIST_SUIT = new HoneyableArmorItem(APIARIST_ARMOR_MATERIAL, ArmorItem.ArmorSlot.CHESTPLATE, new QuiltItemSettings());
 	public static final Item HONEYED_WOOL = new Item(new QuiltItemSettings());
 	@Override
 	public void onInitialize(ModContainer mod)
@@ -28,6 +29,8 @@ public class PollinatorsParadise implements ModInitializer {
 		LOGGER.info("Initialised apiarist veil");
 		Registry.register(Registries.ITEM, new Identifier(ID, "apiarist_wellies"), APIARIST_WELLIES);
 		LOGGER.info("Initialised apiarist wellies");
+		Registry.register(Registries.ITEM, new Identifier(ID, "apiarist_suit"), APIARIST_SUIT);
+		LOGGER.info("Initialised apiarist plate");
 		Registry.register(Registries.ITEM, new Identifier(ID, "honeyed_wool"), HONEYED_WOOL);
 		LOGGER.info("Initialised honeyed wool");
 
