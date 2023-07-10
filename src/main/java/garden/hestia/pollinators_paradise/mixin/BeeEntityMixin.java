889ce7057768def815d18cb74877150084627a89
HestiMae <hestiaheartstrings@gmail.com>
2023-07-10 14:03:29 +1000

    Bees can apply poison on Easy (but not to the player)

--- src/main/java/garden/hestia/pollinators_paradise/mixin/BeeEntityMixin.java
+++ src/main/java/garden/hestia/pollinators_paradise/mixin/BeeEntityMixin.java
@@ -1,15 +1,19 @@
 package garden.hestia.pollinators_paradise.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.passive.BeeEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(BeeEntity.class)
 public abstract class BeeEntityMixin extends LivingEntity {
@@ -41,5 +45,15 @@ public abstract class BeeEntityMixin extends LivingEntity {
 	private StatusEffect strengthAppliesWither(StatusEffect originalEffect)
 	{
 		return (this.hasStatusEffect(StatusEffects.STRENGTH) && this.getStatusEffect(StatusEffects.STRENGTH).getAmplifier() >= 1) ? StatusEffects.WITHER : originalEffect;
+	}
+
+	@ModifyVariable(method = "tryAttack", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getDifficulty()Lnet/minecraft/world/Difficulty;"), ordinal = 0)
+	private int beePoisonOnEasy(int original, Entity target)
+	{
+		if (target instanceof PlayerEntity || this.getWorld().getDifficulty() == Difficulty.PEACEFUL)
+		{
+			return original;
+		}
+		return 5;
 	}
 }
