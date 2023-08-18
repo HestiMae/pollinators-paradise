324c84238a7845a4832a732eb9500162e4eefcae
HestiMae <hestiaheartstrings@gmail.com>
2023-08-18 14:23:37 +1000

    Bees don't get stuck on dripstone blocks

--- src/main/java/garden/hestia/pollinators_paradise/mixin/PointedDripstoneBlockMixin.java
+++ src/main/java/garden/hestia/pollinators_paradise/mixin/PointedDripstoneBlockMixin.java
@@ -1,29 +1,23 @@
 package garden.hestia.pollinators_paradise.mixin;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.EntityShapeContext;
-import net.minecraft.block.PointedDripstoneBlock;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@SuppressWarnings("deprecation")
 @Mixin(PointedDripstoneBlock.class)
-public class PointedDripstoneBlockMixin {
+public class PointedDripstoneBlockMixin extends Block  {
+	public PointedDripstoneBlockMixin(Settings settings) {
+		super(settings);
+	}
 
-	@Inject(method = "getOutlineShape", at = @At("HEAD"), cancellable = true)
-	public void beesDontCollide(BlockState state, BlockView world, BlockPos pos, ShapeContext context, CallbackInfoReturnable<VoxelShape> cir)
-	{
-		if (context instanceof EntityShapeContext entityShapeContext && entityShapeContext.getEntity() instanceof BeeEntity)
-		{
-			cir.setReturnValue(VoxelShapes.empty());
-			cir.cancel();
-		}
+	@Override
+	public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+		if (context instanceof EntityShapeContext entityShapeContext && entityShapeContext.getEntity() instanceof BeeEntity) return VoxelShapes.empty();
+		return super.getCollisionShape(state, world, pos, context);
 	}
 }
